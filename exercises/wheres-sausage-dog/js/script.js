ba94ac2d470dfbcae05eb23036316357c2b77810
jessikaforster <jessikaforster@icloud.com>
2022-01-15 15:27:19 -0500

    E1: Started Where's Sausage Dog? exercise and added assets into folder

--- exercises/wheres-sausage-dog/js/script.js
+++ exercises/wheres-sausage-dog/js/script.js
@@ -8,25 +8,12 @@ author, and this description to match your project!
 
 "use strict";
 
-const NUM_ANIMAL_IMAGES = 10;
-const NUM_ANIMALS = 100;
-
-let animalImages = [];
-let animals = [];
-
-let sausageDogImage = undefined;
-let sausageDog = undefined;
 
 /**
 Description of preload
 */
 function preload() {
-  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
-    let animalImage = loadImage(`assets/images/animal${i}.png`)
-    animalImages.push(animalImage);
-  }
 
-  sausageDogImage = loadImage(`assets/images/sausage-dog.png`);
 }
 
 
@@ -34,20 +21,7 @@ function preload() {
 Description of setup
 */
 function setup() {
-  createCanvas(windowWidth, windowHeight);
 
-  // Create the animals
-  for (let i = 0; i < NUM_ANIMALS; i++) {
-    let x = random(0, width);
-    let y = random(0, height);
-    let animalImage = random(animalImages);
-    let animal = new Animal(x, y, animalImage);
-    animals.push(animal);
-  }
-
-  let x = random(0, width);
-  let y = random(0, height);
-  sausageDog = new SausageDog(x, y, sausageDogImage);
 }
 
 
@@ -55,15 +29,5 @@ function setup() {
 Description of draw()
 */
 function draw() {
-  background(255, 255, 0);
 
-  for (let i = 0; i < animals.length; i++) {
-    animals[i].update();
-  }
-
-  sausageDog.update();
-}
-
-function mousePressed() {
-  sausageDog.mousePressed();
 }
