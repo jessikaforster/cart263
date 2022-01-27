8dc127c84b563f74a428fb14f6621c0826fb0fee
jessikaforster <jessikaforster@icloud.com>
2022-01-27 16:58:33 -0500

    E2: Completed Slamina New Game+ exercise

--- exercises/slamina-new-game-plus/js/Album.js
+++ exercises/slamina-new-game-plus/js/Album.js
@@ -1,3 +1,6 @@
+// Displaying album cover images
+// Adding random movement to album cover images
+
 class Album {
   constructor(x, y, image) {
     this.x = x;
@@ -6,41 +9,37 @@ class Album {
     this.vy = 0;
     this.speed = 1;
     this.image = image;
-    this.angle = 0;
     this.size = 150;
-    this.rotationSpeed = 0.25;
-    this.correct = false;
   }
 
 
-// Displaying fruit images
-update() {
-  this.display();
-  this.move();
-}
-
-display() {
-  push();
-  imageMode(CENTER);
-  translate(this.x, this.y);
-  rotate(this.angle);
-  image(this.image, 0, 0, this.size, this.size);
-  pop();
-}
-
-// Adding movement for album covers
-move() {
-this.x += this.vx;
-this.y += this.vy;
-
-// Adds random movement
-  let change = random(0, 1);
-  if (change < 0.01) {
-    this.vx = random(-this.speed, this.speed);
-    this.vy = random(-this.speed, this.speed);
+  // Displaying album cover images and adding movement
+  update() {
+    this.display();
+    this.move();
   }
-  // Constrains animals to inside the frame
-  this.x = constrain(this.x, 0, width);
-  this.y = constrain(this.y, 0, height);
-}
+
+  // Displaying album cover images
+  display() {
+    push();
+    imageMode(CENTER);
+    image(this.image, this.x, this.y, this.size, this.size);
+    pop();
+  }
+
+  // Adding movement for album covers
+  move() {
+    this.x += this.vx;
+    this.y += this.vy;
+
+    // Adds random movement
+    let change = random(0, 1);
+    if (change < 0.01) {
+      this.vx = random(-this.speed, this.speed);
+      this.vy = random(-this.speed, this.speed);
+    }
+    // Constrains albums to inside the frame
+    this.x = constrain(this.x, 0, width);
+    this.y = constrain(this.y, 0, height);
+  }
 }
--- exercises/slamina-new-game-plus/js/script.js
+++ exercises/slamina-new-game-plus/js/script.js
@@ -1,16 +1,17 @@
 /**
-Title of Project
-Author Name
+Slamina New Game+
+Jessika Forster
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+The goal of this exercise is to learn how to use both annyang! and ResponsiveVoice.
+We must add a minimum of 3 new elements that were not in the original Slamina activity.
 */
 
 "use strict";
 
 let state = `start`;
-// Could be start, simulation, success, failure
+// Could be start, simulation
 
+// List of possible songs that could be said by ResponsiveVoice
 const songs = [
   "one minute back",
   "alarm clock",
@@ -64,19 +65,23 @@ const songs = [
   "why so serious"
 ];
 
+// Variables that will refer to the constant above
 let currentSong = ``;
 let currentAnswer = ``;
 
+// Number of album images to be used
 const NUM_ALBUM_IMAGES = 20;
+// Number of album images to be displayed in total
 const NUM_ALBUMS = 40;
 
+// Defining album image arrays
 let albumImages = [];
 let albums = [];
 
+// Defining image used in `start` state
 let introImage;
 
-let successSFX;
-
+// Defining variables for image displayed in `start` state
 let intro = {
   x: undefined,
   y: undefined,
@@ -85,27 +90,28 @@ let intro = {
 };
 
 /**
-Description of preload
+Loading images to be used into code
 */
 function preload() {
-introImage = loadImage("assets/images/shinee.jpg");
+  // Image used in `start` state
+  introImage = loadImage("assets/images/shinee.jpg");
 
-successSFX = loadSound("assets/sounds/superstar.wav");
-
-  // Loading all 20 song images at once
-    for (let i = 0; i < NUM_ALBUM_IMAGES; i++) {
-      let albumImage = loadImage(`assets/images/album${i}.jpeg`)
-      albumImages.push(albumImage);
-    }
+  // Loading all 20 album cover images at once
+  for (let i = 0; i < NUM_ALBUM_IMAGES; i++) {
+    let albumImage = loadImage(`assets/images/album${i}.jpeg`)
+    albumImages.push(albumImage);
+  }
 }
 
 /**
-Description of setup
+Creating canvas, album cover images will appear randomly, implementing annyang!
+Defining text variables.
 */
 function setup() {
-createCanvas(windowWidth, windowHeight);
+  // Creating canvas to fill full screen
+  createCanvas(windowWidth, windowHeight);
 
-// Create the albums to be displayed randomly and move
+  // Create the albums to be displayed randomly and move
   for (let i = 0; i < NUM_ALBUMS; i++) {
     let x = random(0, width);
     let y = random(0, height);
@@ -114,41 +120,44 @@ createCanvas(windowWidth, windowHeight);
     albums.push(album);
   }
 
-if (annyang) {
-  let commands = {
-    'I think the song is *song': guessSong
-  };
-  annyang.addCommands(commands);
-  annyang.start();
+  // Annyang will listen when this sentence is said
+  if (annyang) {
+    let commands = {
+      'I think the song is *song': guessSong
+    };
+    annyang.addCommands(commands);
+    annyang.start();
 
-  textSize(50);
-  textFont(`Roboto Mono`);
-  textStyle(BOLD);
-  textAlign(CENTER, CENTER);
+    // Defining text variables
+    textSize(50);
+    textFont(`Roboto Mono`);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
   }
 }
 
 
 /**
-Description of draw()
+Defining all states
 */
 function draw() {
-if (state === `start`) {
-  start();
-} else if (state === `simulation`) {
-  simulation();
-} else if (state === `failure`) {
-  failure();
-} else if (state === `success`) {
-  success();
+  if (state === `start`) {
+    start();
+  } else if (state === `simulation`) {
+    simulation();
   }
 }
 
+// `Start` state
 function start() {
+  // Background is dark purple
   background(73, 18, 207);
+  // Displaying intro image
   displayIntro();
+  // Pressing space will trigger `simulation` state
   keyPressed();
-push();
+  // Defining all text variables
+  push();
   textSize(50);
   textFont(`Roboto Mono`);
   textStyle(BOLD);
@@ -158,41 +167,36 @@ push();
   pop();
 }
 
-// (84, 255, 190)
-
+// `Simulation` state
 function simulation() {
+  // Blue background
   background(0, 135, 255);
 
   // Displaying all albums
-    for (let i = 0; i < albums.length; i++) {
-      albums[i].update();
-    }
+  for (let i = 0; i < albums.length; i++) {
+    albums[i].update();
+  }
 
-if (currentAnswer === currentSong) {
-fill(0,255,0);
-}
-else {
-  fill(255,0,0);
-}
-text(currentAnswer, width/2, height/2);
+  // If answer from user is correct, text turns green
+  if (currentAnswer === currentSong) {
+    fill(0, 255, 0);
+  }
+  // If answer is incorrect, text turns red
+  else {
+    fill(255, 0, 0);
+  }
+  // Answer from user will appear at the center of the screen
+  text(currentAnswer, width / 2, height / 2);
 
-
-    push();
-      textSize(50);
-      fill(0);
-      textFont(`Roboto Mono`);
-      textStyle(BOLD);
-      textAlign(CENTER, CENTER);
-      text(`Press 'B' to hear the instructions`, windowWidth / 2, windowHeight / 5);
-      pop();
-}
-
-function failure() {
-background(0, 135, 255);
-}
-
-function success() {
-background(0, 135, 255);
+  // Displaying instructions along with text variables
+  push();
+  textSize(50);
+  fill(0);
+  textFont(`Roboto Mono`);
+  textStyle(BOLD);
+  textAlign(CENTER, CENTER);
+  text(`Press 'B' to hear the instructions`, windowWidth / 2, windowHeight / 5);
+  pop();
 }
 
 // Pressing space triggers `simulation` state
@@ -203,24 +207,26 @@ function keyPressed() {
       state = `simulation`;
     }
   }
-if (keyCode === 66) {
-  responsiveVoice.speak("The song title will be read backwards and you must reply with i think the song is  blank. click anywhere for a song title to be said", "US English Female");
+  // Pressing 'B' will trigger ResponsiveVoice to say instructions
+  if (keyCode === 66) {
+    responsiveVoice.speak("The song title will be read backwards and you must reply with i think the song is  blank. click anywhere for a song title to be said", "US English Female");
   }
 }
 
+// Displaying image used in `start` state at center
 function displayIntro() {
   image(introImage, width / 2, height / 2, intro.width, intro.height);
   imageMode(CENTER, CENTER);
-
-
 }
 
+// Clicking anywhere on the screen will trigger a song to be said backwards
 function mousePressed() {
   currentSong = random(songs);
   let reverseSong = reverseString(currentSong);
   responsiveVoice.speak(reverseSong, "US English Female");
 }
 
+// Function to avoid confusion regarding capitalization
 function guessSong(song) {
   currentAnswer = song.toLowerCase();
 }
