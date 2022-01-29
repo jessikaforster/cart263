6384ebf53768b17ed11c9259bb4da52d922219b4
jessikaforster <jessikaforster@icloud.com>
2022-01-29 17:56:26 -0500

    Completed Spy Profile Generator activity

--- activities/spy-profile-generator/js/script.js
+++ activities/spy-profile-generator/js/script.js
@@ -8,12 +8,24 @@ author, and this description to match your project!
 
 "use strict";
 
+let spyProfile = {
+  name: `**REDACTED**`,
+  alias: `**REDACTED**`,
+  secretWeapons: `**REDACTED**`,
+  password: `**REDACTED**`
+};
+
+let instrumentData = undefined;
+let objectData = undefined;
+let tarotData = undefined;
 
 /**
 Description of preload
 */
 function preload() {
-
+instrumentData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`);
+objectData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`);
+tarotData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`);
 }
 
 
@@ -21,13 +33,53 @@ function preload() {
 Description of setup
 */
 function setup() {
+createCanvas(windowWidth, windowHeight);
 
+let data = JSON.parse(localStorage.getItem(`spy-profile-data`));
+
+if (data !== null) {
+let password = prompt(`Agent! What is your password?!`)
+if (password === data.password) {
+  spyProfile.name = data.name;
+  spyProfile.alias = data.alias;
+  spyProfile.secretWeapons = data.secretWeapons;
+  spyProfile.password = data.password;
+}
+  }
+else {
+  generateSpyProfile();
+  }
 }
 
+function generateSpyProfile() {
+  spyProfile.name = prompt(`Agent! What is your name?!`);
+  let instrument = random(instrumentData.instruments);
+  spyProfile.alias = `The ${instrument}`;
+  spyProfile.secretWeapons = random(objectData.objects);
+  let card = random(tarotData.tarot_interpretations);
+  spyProfile.password = random(card.keywords);
+
+  localStorage.setItem(`spy-profile-data`,JSON.stringify(spyProfile));
+}
 
 /**
 Description of draw()
 */
 function draw() {
+background(255);
 
+let profile = `** SPY PROFILE! DO NOT DISTRIBUTE! **
+
+Name: ${spyProfile.name}
+Alias: ${spyProfile.alias}
+Secret Weapon: ${spyProfile.secretWeapons}
+Password: ${spyProfile.password}`;
+
+push();
+textFont(`Courier, monospace`);
+textSize(24);
+textAlign(LEFT, TOP);
+fill(0);
+text(profile, 100, 100);
+pop();
 }
