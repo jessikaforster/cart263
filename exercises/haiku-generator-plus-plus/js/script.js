4cfe0713bc5ad632e196d21af508df02f7bc1aab
jessikaforster <jessikaforster@icloud.com>
2022-02-22 20:09:08 -0500

    E5: Changed variables

--- exercises/haiku-generator-plus-plus/js/script.js
+++ exercises/haiku-generator-plus-plus/js/script.js
@@ -7,32 +7,99 @@ Generates a random haiku!
 
 "use strict";
 
-let fiveSyllableLines = [
-  `O, to be a tree`,
-  `The cat does not know`,
-  `We are all forests`,
-  `You have done your best`,
-  `They are all gone now`
+let months = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`
 ];
-let sevenSyllableLines = [
-  `Say the things left unsaid`,
-  `Never believe the wind's lies`,
-  `The autumn stretches its legs`,
-  `Nothing can satisfy you`,
-  `They will not come back again`
+let days = [
+  `1st`,
+  `2nd`,
+  `3rd`,
+  `4th`,
+  `5th`,
+  `6th`,
+  `7th`,
+  `8th`,
+  `9th`,
+  `10th`,
+  `11th`,
+  `12th`,
+  `13th`,
+  `14th`,
+  `15th`,
+  `16th`,
+  `17th`,
+  `18th`,
+  `19th`,
+  `20th`,
+  `21st`,
+  `22nd`,
+  `23rd`,
+  `24th`,
+  `25th`,
+  `26th`,
+  `27th`,
+  `28th`
+];
+let years = [
+  `2023`,
+  `2024`,
+  `2025`,
+  `2026`,
+  `2027`,
+  `2028`,
+  `2029`,
+  `2030`,
+  `2031`,
+  `2032`,
+  `2033`,
+  `2034`,
+  `2035`,
+  `2036`,
+  `2037`,
+  `2038`,
+  `2039`,
+  `2040`
+];
+let action = [
+  `Get married`,
+  `Fall in love`,
+  `Meet your idol`,
+  `Take a risk`,
+  `Face your fear`,
+  `Get a pet`,
+  `Try something new`,
+  `Reconnect with an old friend`,
+  `Visit a new country`
 ];
 
-let line1 = random(fiveSyllableLines);
-let line2 = random(sevenSyllableLines);
-let line3 = random(fiveSyllableLines);
+let line1 = random(months);
+let line2 = random(days);
+let line3 = random(years);
+let line4;
+let line5 = random(action);
 
 let line1P = document.getElementById(`line-1`);
 let line2P = document.getElementById(`line-2`);
 let line3P = document.getElementById(`line-3`);
+let line4P = document.getElementById(`line-4`);
+let line5P = document.getElementById(`line-5`);
 
 line1P.innerText = line1;
 line2P.innerText = line2;
 line3P.innerText = line3;
+//line4P.innerText = line4;
+line5P.innerText = line5;
 
 line1P.addEventListener(`click`, lineClicked);
 line2P.addEventListener(`click`, lineClicked);
