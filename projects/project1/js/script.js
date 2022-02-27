ef80e681a9d602851a0c2a4bf2a8fc64a5bf61aa
jessikaforster <jessikaforster@icloud.com>
2022-02-27 13:57:19 -0500

    P1: Created all states

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,13 +1,14 @@
 /**
-Title of Project
-Author Name
+Train to Busan
+Jessika Forster
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Project 1, mid-term CART 263 project. View README.md for artist's statement.
 */
 
 "use strict";
 
+let state = `start`;
+/* Could be start, level1, level2, level2Fail, level3, level3Fail, level3Success */
 
 /**
 Description of preload
@@ -29,5 +30,47 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  if (state === `start`) {
+    start();
+  } else if (state === `level1`) {
+    level1();
+  } else if (state === `level2`) {
+    level2();
+  } else if (state === `level2Fail`) {
+    level2Fail();
+  } else if (state === `level3`) {
+    level3();
+  } else if (state === `level3Fail`) {
+    level3Fail();
+  } else if (state === `level3Success`) {
+    level3Success();
+  }
+}
+
+function start() {
+
+}
+
+function level1() {
+
+}
+
+function level2() {
+
+}
+
+function level2Fail() {
+
+}
+
+function level3() {
+
+}
+
+function level3Fail() {
+
+}
+
+function level3Success() {
 
 }
