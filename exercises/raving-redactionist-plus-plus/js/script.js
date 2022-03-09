ea03b1a639c552ec0e843454733afc18bde94437
jessikaforster <jessikaforster@icloud.com>
2022-03-08 23:46:04 -0500

    E6: Added comments

--- exercises/raving-redactionist-plus-plus/js/script.js
+++ exercises/raving-redactionist-plus-plus/js/script.js
@@ -2,30 +2,36 @@
 Raving Redactionist++
 Jessika Forster
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+In this game, the romanized lyrics of a Korean song are displayed and you
+must click on the covered letters in order to reveal the secret message. You can
+press the play button to listen to the song as you work to reveal the message.
 */
 
 "use strict";
 
+// Playing the song
 function play() {
   var audio = new Audio('assets/sounds/music.wav');
   audio.play();
 }
 
+// Clicking will reveal the covered letters
 $(`.top-secret`).on(`click`, reveal);
 setInterval(revelation, 500);
 
+// Function that will switch letters from being covered to revealed
 function reveal(event) {
   $(this).removeClass(`redacted`);
   $(this).addClass(`revealed`);
 }
 
+// Triggering function that will cover letters
 function revelation() {
-  $(`.revealed`).each(attemptReveal);
+  $(`.revealed`).each(attemptRedact);
 }
 
-function attemptReveal() {
+// Randomly putting white back over letters
+function attemptRedact() {
   let r = Math.random();
   if (r < 0.1) {
     $(this).removeClass(`revealed`);
