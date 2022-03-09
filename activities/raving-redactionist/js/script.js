a3f41e57a4da69af760eddd3b66a59bb44ee0cf8
jessikaforster <jessikaforster@icloud.com>
2022-03-08 21:23:15 -0500

    A: Added revelation and attemptReveal functions

--- activities/raving-redactionist/js/script.js
+++ activities/raving-redactionist/js/script.js
@@ -8,4 +8,16 @@ author, and this description to match your project!
 
 "use strict";
 
-// Code goes here
+setInterval(revelation, 500);
+
+function relevation() {
+  $(`.redacted`).each(attemptReveal);
+}
+
+function attemptReveal() {
+  let r = Math.random();
+  if (r < 0.1) {
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`revealed`);
+  }
+}
