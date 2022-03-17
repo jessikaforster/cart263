610081cc1e6baa7367b840b803dceb5039269bf4
jessikaforster <jessikaforster@icloud.com>
2022-03-17 18:40:18 -0400

    E7: Started Exercise 7

---
+++ exercises/code-taker-plus-plus/js/script.js
@@ -0,0 +1,37 @@
+/**
+Code Taker
+Jessika Forster
+
+Game to figure out the secret word
+*/
+
+"use strict";
+
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  button: {
+    "I know.": function() {
+      $(this).dialog(`close`);
+    }
+  }
+});
+
+$(`.secret`).one(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({
+    helper: `clone`
+  });
+});
+
+$(`#answer`).droppable({
+  drop: function(event, ui) {
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+    // Check if they got it
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  }
+});
