bb6bdeb52fccdbd0f60843c4c6a10dc0838997fd
jessikaforster <jessikaforster@icloud.com>
2022-03-22 23:23:57 -0400

    P2-P: Added notes and completed prototype

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -4,13 +4,14 @@ Jessika Forster
 
 The prototype I created as a representation of the game I wish to make is a
 game where you must run your mouse over the text in order for keywords to be
-highlighted and used in the following level.
+highlighted and used in the following level. There is 1 highlighted word per
+paragraph.
 */
 
 "use strict";
 
 
-// Mouseover letter in secret word will make it change colour
+// When mouse hovers over specific words they will be highlighted
 $(`.answer`).one(`mouseover`, function(event) {
   $(this).addClass(`found`, 500);
-  });
+});
