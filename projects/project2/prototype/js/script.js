ced31deb5578415cd7f1e95b476d7c3234ed1374
jessikaforster <jessikaforster@icloud.com>
2022-03-22 20:51:12 -0400

    P2-P: Word is highlighted when hovered over

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -1,11 +1,16 @@
 /**
-Title of Project
-Author Name
+Project 2 Prototype
+Jessika Forster
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+The prototype I created as a representation of the game I wish to make is a
+game where you must run your mouse over the text in order for keywords to be
+highlighted and used in the following level.
 */
 
 "use strict";
 
-// Code goes here
+
+// Mouseover letter in secret word will make it change colour
+$(`.answer`).one(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+  });
