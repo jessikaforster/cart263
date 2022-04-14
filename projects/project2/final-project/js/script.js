8af53ba6466afa44f2336f968d0c30594424daed
jessikaforster <jessikaforster@icloud.com>
2022-04-14 12:26:18 -0400

    P2: Duplicated prototype folder for final project

---
+++ projects/project2/final-project/js/script.js
@@ -0,0 +1,17 @@
+/**
+Project 2 Prototype
+Jessika Forster
+
+The prototype I created as a representation of the game I wish to make is a
+game where you must run your mouse over the text in order for keywords to be
+highlighted and used in the following level. There is 1 highlighted word per
+paragraph.
+*/
+
+"use strict";
+
+
+// When mouse hovers over specific words they will be highlighted
+$(`.answer`).one(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+});
